776e32ba534df1310091546414c0df3c2bb57153
javad yosuefi <javadyousefi9000@gmail.com>
2023-11-21 17:52:37 -0900

    bye next

--- pages/posts/index.js
+++
@@ -1,6 +0,0 @@
-const MainPost = () => {
-    return <>main post page will show if you dont enter a id on url</>;
-  };
-  
-  export default MainPost;
-  
---
+++ pages/posts/post.js
@@ -0,0 +1,5 @@
+const Main = () => {
+  return <>tesF</>;
+};
+
+export default Main;
