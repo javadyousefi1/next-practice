5c20e248690ba13663b81385a436a5611bbf0209
javad yosuefi <javadyousefi9000@gmail.com>
2023-11-21 17:28:49 -0900

    catch route

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,6 @@
 const MainPost = () => {
-  return <>main post page will show if you dont enter a id on url</>;
-};
-
-export default MainPost;
+    return <>main post page will show if you dont enter a id on url</>;
+  };
+  
+  export default MainPost;
+  
