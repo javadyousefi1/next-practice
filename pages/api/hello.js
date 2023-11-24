43211dacc4333efae93114fb9476d8fa97ffebb6
javad yosuefi <javadyousefi9000@gmail.com>
2023-11-24 14:21:37 -0900

    redirecet user from next backend

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe',id:"tes" })
+  if (req.method === "POST") {
+    // res.status(200).json({ name: "John Doe", id: "tes" });
+    res.status(200).json({ redirectUrl: "/" });
+    // Process a POST request
+  } else {
+    // Handle any other HTTP method
+  }
 }
--- pages/useApi/index.js
+++ pages/useApi/index.js
@@ -1,10 +1,29 @@
 import axios from "axios";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 const Index = () => {
+  const router = useRouter();
+
   useEffect(() => {
-    axios.get("http://localhost:3000/api/hello");
-  }, []);
+    axios.post("/api/hello") // Use relative path to the API route
+      .then((response) => {
+        // Check if the response contains a redirect URL
+        const redirectUrl = response.data.redirectUrl;
+
+        if (redirectUrl) {
+          // Use the Next.js router to navigate to the specified URL
+          router.replace(redirectUrl);
+        } else {
+          // Handle other response scenarios
+          console.log(response);
+        }
+      })
+      .catch((error) => {
+        // Handle errors
+        console.error(error);
+      });
+  }, [router]);
 
   return <>test</>;
 };
